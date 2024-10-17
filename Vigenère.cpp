bb49216ac2a3fab9db4665cedd9839d67f100e60
Skad <notcarolinna@gmail.com>
2024-10-17 06:46:20 -0300

    Tabela de frequências

--- Vigenère.cpp
+++ Vigenère.cpp
@@ -3,9 +3,36 @@
 #include <fstream>
 #include <unordered_map>
 #include <vector>
-#include <algorithm> 
+#include <algorithm>
 #include <set>
 #include <map>
+#include <sstream>
+
+std::map<char, std::string> frequencyReader(const std::string& filePath) {
+    std::ifstream file(filePath);
+    std::map<char, std::string> frequencyTable;
+
+    if (!file) {
+        std::cerr << "Unable to open file: " << filePath << std::endl;
+        return frequencyTable;
+    }
+
+    std::string line;
+
+    while (std::getline(file, line)) {
+        char letter;
+        std::string frequency;
+        std::string trash;
+
+        std::istringstream iss(line);
+        iss >> letter >> trash >> frequency;
+
+
+        frequencyTable[letter] = frequency;
+    }
+
+    return frequencyTable;
+}
 
 std::set<int> getDivisors(int n) {
     std::set<int> divisors;
@@ -17,7 +44,7 @@ std::set<int> getDivisors(int n) {
     return divisors;
 }
 
-std::map<int, int> findDivisorFrequencies(const std::string& cipher, int patternLength) {
+std::map<int, int> divisorFrequencies(const std::string& cipher, int patternLength) {
     std::unordered_map<std::string, std::vector<int>> patternPositions;
     std::map<int, int> divisorFrequency;
 
@@ -42,22 +69,37 @@ std::map<int, int> findDivisorFrequencies(const std::string& cipher, int pattern
     return divisorFrequency;
 }
 
-int main() {
-    std::ifstream cipherFile("./resources/teste-eng.txt");
-    std::string cipher;
+std::string cipherReader(const std::string& filePath) {
+    std::ifstream file(filePath);
+    std::string content;
 
-    if (!cipherFile) {
-        std::cerr << "Unable to open cipherFile" << std::endl;
-        return 1;
+    if (!file) {
+        std::cerr << "Unable to open file: " << filePath << std::endl;
+        return "";
     }
 
     std::string line;
-    while (std::getline(cipherFile, line)) {
-        cipher += line;
+    while (std::getline(file, line)) {
+        content += line;
+    }
+
+    return content;
+}
+
+
+int main() {
+    std::string cipher = cipherReader("./resources/teste-eng.txt");
+    if (cipher.empty()) {
+        return 1;
+    }
+
+    std::map<char, std::string> frequencyTable = frequencyReader("./resources/frequency.txt");
+    if (frequencyTable.empty()) {
+        return 1;
     }
 
     int patternLength = 4;
-    std::map<int, int> divisorFrequency = findDivisorFrequencies(cipher, patternLength);
+    std::map<int, int> divisorFrequency = divisorFrequencies(cipher, patternLength);
 
     std::vector<std::pair<int, int>> sortedDivisors(divisorFrequency.begin(), divisorFrequency.end());
     std::sort(sortedDivisors.begin(), sortedDivisors.end(), [](const auto& a, const auto& b) {
@@ -66,11 +108,16 @@ int main() {
 
     std::cout << "\n\t\t***Most frequent divisors***\n\n";
     int count = 0;
-    for (size_t i = 1; i < sortedDivisors.size() && count < 10; ++i) {
+    for (size_t i = 0; i < sortedDivisors.size() && count < 10; ++i) {
         std::cout << "Divisor: " << sortedDivisors[i].first << " - Frequency: " << sortedDivisors[i].second << '\n';
         count++;
     }
-    std::cout << "\n**********************************************************\n";
+    std::cout << "\n**********************************************************\n\n";
+
+    for (const auto& pair : frequencyTable) {
+    std::cout << "Letter: " << pair.first << " - Frequency: " << pair.second << std::endl;
+    }
 
     return 0;
 }
+
